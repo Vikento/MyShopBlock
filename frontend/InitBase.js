4a1d7e5a43f89e22d837fed92ec187498a040f2c
unknown <vtoramanian@gmail.com>
2020-06-15 16:00:15 +0000

    Add new file and Pictures for the creation of the creation of the Guide and help user to avoid mistake

---
+++ frontend/InitBase.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+ 
+
+ 
+class InitBase extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Integration of My Block Stock</h1>
+        <h2>I/ Importation of the Block from GitHub </h2>
+        <p> </p>
+        <p>We will how first how to integrate the module Block Stock from GitHub</p>
+
+        <p> </p>  <p> </p>  <p> </p>
+        <h2>II/ Creation of the Table for the best use </h2>
+        <p> </p>  <p> </p>  <p> </p>
+        <h3>1. List of tables and fields to create</h3>
+    
+         <p> </p>  <p> </p>  <p> </p>
+        <h3>2. Rules to respect</h3>
+        
+        <p> </p>  <p> </p>  <p> </p>
+        <h3>3. Personalyzed the Block</h3>
+        <p> </p>  <p> </p>  <p> </p>
+
+      </div>
+    );
+  }
+}
+ 
+export default InitBase;
